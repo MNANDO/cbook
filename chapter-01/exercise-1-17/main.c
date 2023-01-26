d9fad03e9d9cb8dffd0c4a7f6813e09abf8ff6a4
Michael F Hernando <mfh23@afsconnect1.njit.edu>
2023-01-25 23:22:16 -0500

    exercise 17 complete

---
+++ chapter-01/exercise-1-17/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define MINLINE 80
+#define MAXLINE 1000
+
+int get_line(char line[], int maxline);
+
+// write a program to print all input lines that are longer than 80 characters
+int main() {
+    int len;                /* current line length */
+    char line[MAXLINE];
+
+    while ((len = get_line(line, MAXLINE)) > 0) {
+        if (len > MINLINE) {
+            printf("%s", line); 
+        } 
+    }
+
+    return 0;
+}
+
+/* getline: read a line into s, return length */
+int get_line(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) 
+        s[i] = c;
+
+    if (c == '\n') {
+        s[i] = c; 
+        ++i;
+    }
+
+    s[i] = '\0';
+
+    return i;
+}
